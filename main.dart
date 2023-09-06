66e6db89922b4f7bd1157d173b8af7e6ee767d8d
Durdona <durdonajurakulova98@gmail.com>
2023-09-06 08:58:03 +0500

    first commit

--- main.dart
+++ main.dart
@@ -5,44 +5,62 @@ import 'dart:io';
 
 void main(List<String> args) 
 {
-//   print('Enter A:');
+  // <----------1---------->
+  // final  data = stdin.readLineSync();
 
-//   final int a  = int.parse(stdin.readLineSync() ?? '0'); 
-
-// print("A is $a");
-// print('Enter A:');
-//  final int data = int.parse(stdin.readLineSync() ?? '0');
-//  print('Enter B:');
-//   final int data1 = int.parse(stdin.readLineSync() ?? '0');
-
-//  if(data > data1) {
-//   print("$data больше чем $data1 ");
-//  } else if (data1 > data){
-//   print("$data1 больше чем $data ");
-//  } else if (data == data1){
-//   print("$data равны $data1 ");
-// }
-  final List arr = [];
-  
-print('Enter A:');
- final int data = int.parse(stdin.readLineSync() ?? '0');
- print('Enter B:');
-  final int data1 = int.parse(stdin.readLineSync() ?? '0');
-  arr.add(data);
-  arr.add(data1);
-  print(arr);
-
- if(arr[0] > arr[1]) {
-  print("$data больше чем $data1 ");
- } else if (arr[0] < arr[1]){
-  print("$data1 больше чем $data ");
- } else if (arr[0] == arr[1]){
-  print("равны");
-}
-
-  
-}
-
-//
+  // if (data == 'Alex' || data == "alex" ) {
+  //   final dataage = int.parse(stdin.readLineSync() ?? '0');
+  //   if (dataage >= 18 && dataage < 30) {
+  //       final int dataDol = int.parse(stdin.readLineSync() ?? '0');
+  //   if(dataDol >= 100) {
+  //     print("Здорово братан у тя какое-то скучное имя (потом переименуй) и welcome в вечеринку");
+  //   } else {
+  //     print("А пошёл ты нафиг братанчик");
+  //   }
+  //   } else {
+  //     print("Старпёр");
+  //   }
  
-//}
+  // } else {
+  //   print("Нет не мы сюда пропускаем Алексов и богатых");
+  // };
+  // <------------1----------->
+
+
+
+ // <----------2----------->
+  // final int info = int.parse(stdin.readLineSync() ?? '0');
+
+  // if(info >= 90 && info <= 100) {
+  //   print("Bravo");
+  // } else if (info >= 80 && info <= 89) {
+  //   print("Good job man");
+  // } else if (info >= 70 && info <= 79) {
+  //   print("Под пивас сойдет");
+  // } else if (info >= 60 && info <= 69) {
+  //   print("Acceptable bro");
+  // } else if (info < 60 && info >= 30) {
+  //   print("O my god what is this");
+  // } else if (info < 30 && info >= 10) {
+  //   print("тошнит");
+  // } else if (info < 10) {
+  //   print("У тя чё-то не то с числами");
+  // }
+  // <----------2----------->
+
+   
+   // <----------3---------->
+  //  final List arr = ["Alex"];
+  //  final quest = stdin.readLineSync();
+
+  //  if(quest == arr[0]) {
+  //   print("Всё OK"); 
+   
+  //  } else  {
+  //   print("No lie");
+  //  }
+
+   // <----------3------------>
+  
+
+}
